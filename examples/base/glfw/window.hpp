af496c255a5b149666abda4e1ceb1a1d9b662dc7
Scott Bennett <scottbennett912@gmail.com>
2021-04-10 12:20:03 -0400

    Enable glfw scale to monitor

--- examples/base/glfw/window.hpp
+++ examples/base/glfw/window.hpp
@@ -25,15 +25,14 @@ public:
 
     glfwDefaultWindowHints();
 
-    uint32_t flags = 0;  // SDL_WINDOW_HIDDEN;
     if constexpr (std::is_same<Ctx, crystal::opengl::Context>::value) {
       glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
       glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
       glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
       glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, 1);
       glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-      glfwWindowHint(GLFW_SRGB_CAPABLE, 1);
-      glfwWindowHint(GLFW_DOUBLEBUFFER, 1);
+      glfwWindowHint(GLFW_SRGB_CAPABLE, GLFW_TRUE);
+      glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
       glfwWindowHint(GLFW_SAMPLES, 4);
     }
     if constexpr (std::is_same<Ctx, crystal::vulkan::Context>::value) {
@@ -44,6 +43,7 @@ public:
     }
 
     glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
     glfwSwapInterval(0);
 
     GLFWwindow* window = glfwCreateWindow(width, height, title, nullptr, nullptr);
